5a67c7152092b343902268d6704161f386699054
Rodrigo Andrade <soadse.ra@gmail.com>
2024-11-27 22:04:34 -0300

    teste

---
+++ js.js
@@ -0,0 +1,59 @@
+let lista = []
+let busca = ''
+
+let inputNomesCadastro = document.getElementById('inputNomesCadastro')
+let btnCadastro = document.getElementById('btnCadastro')
+
+let inputNomesPesquisa = document.getElementById('inputNomesPesquisa')
+let btnBusca = document.getElementById('btnBusca')
+
+let btnMostrarLista = document.getElementById('btnMostrarLista')
+let resultadoDeBusca = document.getElementById('resultadoDeBusca')
+
+inputNomesCadastro.addEventListener('click', () => {
+    resultadoDeBusca.innerHTML = ''
+})
+inputNomesPesquisa.addEventListener('click', () => {
+    resultadoDeBusca.innerHTML = ''
+})
+
+btnCadastro.addEventListener('click', () => {
+    if(inputNomesCadastro.value === ''){
+        resultadoDeBusca.innerHTML = 'Campo de cadastro vazio<br>Insira uma palavra'
+    } else {
+        lista.push(inputNomesCadastro.value)
+        inputNomesCadastro.value = ''
+    }
+})
+
+btnBusca.addEventListener('click', () => {
+    if(lista.includes(inputNomesPesquisa.value)){
+        resultadoDeBusca.innerHTML = `Palavra encontrada: ${inputNomesPesquisa.value}`
+        inputNomesPesquisa.value = ''
+    } else if(inputNomesPesquisa.value === '') {
+        resultadoDeBusca.innerHTML = 'Campo de busca vazio<br>Insira uma palavra'
+    } else {
+        resultadoDeBusca.innerHTML = `Palavra não encontrada: ${inputNomesPesquisa.value}`
+        inputNomesPesquisa.value = ''
+    }
+})
+
+btnMostrarLista.addEventListener('click', () => {
+    if(lista.length === 0){
+        resultadoDeBusca.innerHTML = 'Lista vazia'
+    } else {
+        resultadoDeBusca.innerHTML = lista
+    }
+})
+
+
+
+
+
+
+
+// if(lista.includes(nome)){
+//     div2.innerHTML = `Nome localizado: ${nome}`
+// } else {
+//     div2.innerHTML = `Nome não localizado: ${nome}`
+// }
